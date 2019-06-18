a512bd3ed2a915d46abca698995953c6b838406c
netsun <303078098@qq.com>
2019-06-18 18:10:01 +0800

    test: 创建钱包地址

--- example/main.go
+++ example/main.go
@@ -10,7 +10,11 @@ import (
 func main() {
 	factory := wallet.Factory{Environment: setting.Env_DEVELOPMENT}
 	usdt := factory.Get("USDT")
+
 	balance := usdt.GetBalance("mt8Lvqmik6w4ZimnqDb8pKUupYyXFpjBQJ")
 	log.Printf("查询余额：%s\n", balance)
+
+	//address := usdt.NewAddress("")
+	//log.Printf("钱包地址：%s\n", address)
 
 }
